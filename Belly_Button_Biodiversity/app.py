a818364280040d90ddac092b8191d8abb06e9383
Bowie Hand <bowielh@hotmail.com>
2018-10-28 17:22:02 -0500

    Alter import statements to remove preceeding periods.

--- Belly_Button_Biodiversity/app.py
+++ Belly_Button_Biodiversity/app.py
@@ -30,7 +30,7 @@ db = SQLAlchemy(app)
 # Samples_Metadata = Base.classes.sample_metadata
 # Samples = Base.classes.samples
 
-from .models import Samples_Metadata, Samples
+from models import Samples_Metadata, Samples
 
 @app.route("/")
 def index():
